0a981acafd83f664f763f2b5e689fcae7cfabb0b
phurley <phurley@gmail.com>
2026-08-19 19:49:55 -0400

    Use literal URLs (script-generated form) instead of Ruby interpolation

--- Formula/wless.rb
+++ Formula/wless.rb
@@ -6,22 +6,22 @@ class Wless < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/phurley/wless/releases/download/v#{version}/wless-v#{version}-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/phurley/wless/releases/download/v1.3.0/wless-v1.3.0-aarch64-apple-darwin.tar.gz"
       sha256 "9e4f9574ba210ce0d4bef92f91a94602388c037b3787b6d049b1a505e961c34c"
     end
     on_intel do
-      url "https://github.com/phurley/wless/releases/download/v#{version}/wless-v#{version}-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/phurley/wless/releases/download/v1.3.0/wless-v1.3.0-x86_64-apple-darwin.tar.gz"
       sha256 "ba48df2bf5a63459fcdaa9fefae6572d78e47f81715f6a42ec9ef7fddd8522de"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/phurley/wless/releases/download/v#{version}/wless-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/phurley/wless/releases/download/v1.3.0/wless-v1.3.0-aarch64-unknown-linux-gnu.tar.gz"
       sha256 "0d0a3148c28b027a160efe8cb483307363ff58831cb6c2f8e68b813ed5e0d661"
     end
     on_intel do
-      url "https://github.com/phurley/wless/releases/download/v#{version}/wless-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/phurley/wless/releases/download/v1.3.0/wless-v1.3.0-x86_64-unknown-linux-gnu.tar.gz"
       sha256 "26f7ce6a4e55a510f5f0d2bf6e422b3bb4f18da5a63e90f71240dd13a1b1543c"
     end
   end
